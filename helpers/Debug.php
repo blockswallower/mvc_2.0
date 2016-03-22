df9ea5aed78f399d5cc822b15e362d9ec4625145
Dennis <dennisslimmers@live.nl>
2016-03-22 19:38:16 +0100

    Added debig helper class

---
+++ helpers/Debug.php
@@ -0,0 +1,20 @@
+<?php
+
+class Dedug {
+    /**
+     * This class will be used for several
+     * debug methods
+     */
+
+    /**
+     * $param $data
+     *
+     * dumps data between
+     * <pre> tags
+     */
+    public static function dump($data) {
+        echo '<pre>';
+        var_dump($data);
+        echo '</pre>';
+    }
+}
