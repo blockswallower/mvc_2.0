e43c804046eda1e23e24d10fd8f124d6a2348409
Dennis <dennisslimmers@live.nl>
2016-03-22 15:25:49 +0100

    Created improved autoloader

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
 
-spl_autoload_register(function($class){
-    require 'app/'.$class.'.php';
-});
+/**
+ * @var array
+ *
+ * store every directory
+ * you want to autoload in
+ * this array
+ */
+$directories = array(
+    './app',
+    './helpers'
+);
+
+/**
+ * @param $directory
+ *
+ * autoloads every directory
+ * in the $directories array
+ */
+function autoload($directories) {
+    foreach ($directories as $dir) {
+        if (!is_dir($dir)) {
+            exit($dir.' is not a directory!');
+        }
+
+        foreach (scandir($dir) as $file) {
+            if ('.' === $file)
+                continue;
+
+            if ('..' === $file)
+                continue;
+
+            require $dir.'/'.$file;
+        }
+    }
+}
+
+autoload($directories);
 
 $app = new Application();
+
