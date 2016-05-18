d2d00b329cd693eaae388a3259c9ebb480f34fe4
Dennis <dennisslimmers@live.nl>
2016-05-18 09:38:58 +0200

    Added String class and lastchar method

---
+++ helpers/String.php
@@ -0,0 +1,28 @@
+<?php
+
+class String {
+	/**
+	 * This class will contain
+	 * string methods
+	 */
+
+	/**
+	 * @param $string
+	 * @param $char
+	 * @return boolean
+	 *
+	 * Returns true if the last character
+	 * of the given string is the same as the
+	 * given character
+	 */
+	public static function lastchar($string, $char) {
+		$string = str_split($string);
+		$last_char = $string[count($string) -1];
+		
+		if ($last_char == $char) 
+			return true;
+		 else 
+			return false;
+		
+	}
+}
