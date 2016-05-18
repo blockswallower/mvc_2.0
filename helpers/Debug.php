5d3f316b9c24cd8c2ac11413da51f4d5c053a8aa
Dennis <dennisslimmers@live.nl>
2016-05-18 11:07:56 +0200

    Added exitdump method to Debug class

--- helpers/Debug.php
+++ helpers/Debug.php
@@ -14,7 +14,22 @@ class Dedug {
      */
     public static function dump($data) {
         echo '<pre>';
-        var_dump($data);
+            var_dump($data);
         echo '</pre>';
+    }
+
+    /**
+     * @param $data
+     *
+     * dumps data between
+     * <pre> tags and ends
+     * the programme
+     */
+    public static function exitdump($data) {
+        echo '<pre>';
+            var_dump($data);
+        echo '</pre>';
+        
+        exit;
     }
 }
