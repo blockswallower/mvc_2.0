694eeeabb95328123543de8fb0470951a4c43f7e
Dennis <dennisslimmers@live.nl>
2016-05-18 10:59:31 +0200

    Added Arrays class for array helper methods

---
+++ helpers/Arrays.php
@@ -0,0 +1,30 @@
+<?php
+
+class Arrays {
+	/**
+	 * This class will contain
+	 * array methods
+	 */
+
+	/**
+	 * @param $array
+	 * @return array
+	 *
+	 * Returns first array item
+	 * of the given array
+	 */
+	public static function first($array) {
+		return $array[0];
+	}
+
+	/**
+	 * @param $array
+	 * @return array
+	 *
+	 * Returns last array item
+	 * of the given array
+	 */
+	public static function last($array) {
+		return $array[count($array) -1];
+	}
+}
