bf19fb113ce2ece4b40155c421a84fe3a46550e6
blockswallower <dennisslimmers@live.nl>
2016-07-31 12:58:27 +0200

    Created javascript clone of the php config array

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,4 +1,11 @@
 
     <?php Link::script('script.js'); ?>
+
+    <script type="text/javascript">
+        config = {};
+        config.debug = true;
+        config.every_time_execution = "none";
+        config.one_time_execution = "none";
+    </script>
     </body>
 </html>
