a60c613672fd129b6f46385ec43e37a0faf0d2f1
Dennis <dennisslimmers@live.nl>
2016-09-30 17:24:20 +0200

    Added possibility to separately extend files & code cleanup view.php

--- app/View.php
+++ app/View.php
@@ -33,43 +33,10 @@ class View {
 	protected $rendered;
 
 	/**
-	 * This method renders
-	 * the requested view
-	 * from the controller
-	 */
-	public function show($view) {
-		$this->require_header();
-		$this->render_view($view);
-		$this->require_footer();
-	}
-
-	/**
-	 * Requires standard header file
-     */
-	public function require_header() {
-		require $this->header;
-	}
-
-	/**
-	 * Requires standard footer file
-     */
-	public function require_footer() {
-		require $this->footer;
-	}
-
-	/**
-	 * Renders the content
-	 * from the requested view
-	 */
-	public function render_view($view) {
-		require $this->basepath.$view.".php";
-	}
-
-	/**
 	 * @param null $key
 	 * @return mixed
 	 *
-	 * returns a value from the view "var" variable
+	 * returns a value from the "var" variable
 	 * can be used in views like this:
 	 *
 	 * $this->get([KEY]);
@@ -90,12 +57,117 @@ class View {
 					Debug::pagedump('The value you are trying to access is empty or NULL');
 				}
 			} else {
-				Debug::pagedump('Please enter a value as an argument: $this->get([VALUE])');
+				Debug::pagedump('Please enter an array key as an argument: $this->get([KEY])');
 			}
 		} else {
 			Debug::pagedump("No variables has been send to this controller yet: " . ucfirst($cur_controller) . "Controller");
 		}
 
 		return $key;
+	}
+
+	/**
+	 * @param $view
+	 *
+	 * This method renders
+	 * the requested view
+	 * from the controller
+	 * with header and footer
+	 * included
+	 */
+	public function show_hf($view) {
+		if (file_exists($this->get_view_path($view))) {
+			if (file_exists($this->header)) {
+				if (file_exists($this->footer)) {
+					$this->require_header();
+					$this->render_view($view);
+					$this->require_footer();
+				} else {
+					Debug::pagedump("The view: '$this->footer' does not exist");
+				}
+			} else {
+				Debug::pagedump("The view: '$this->header' does not exist");
+			}
+		} else {
+			Debug::pagedump("The view: '". $this->get_view_path($view) ."' does not exist");
+		}
+	}
+
+	/**
+	 * @param $view
+	 *
+	 * Does the same as '$this->show([VIEW])'
+	 * but this naming convention is more
+	 * readable if used in a view
+     */
+	public function extend($view) {
+		if (file_exists($this->get_view_path($view))) {
+			include_once $this->get_view_path($view);
+		} else {
+			Debug::pagedump("The view '". $this->get_view_path($view) ."' you are trying to extend
+							 does not exist");
+		}
+	}
+
+	/**
+	 * @param $view
+	 *
+	 * This method renders
+	 * the requested view
+	 * from the controller
+	 */
+	public function show($view) {
+		if (file_exists($this->get_view_path($view))) {
+			$this->render_view($view);
+		} else {
+			Debug::pagedump("The view '". $this->get_view_path($view) ."' does not exist");
+		}
+	}
+
+	/**
+	 * Requires standard header file
+	 */
+	public function require_header() {
+		if (file_exists($this->header)) {
+			require $this->header;
+		} else {
+			Debug::pagedump("The view: '$this->header' does not exist");
+		}
+	}
+
+	/**
+	 * Requires standard footer file
+	 */
+	public function require_footer() {
+		if (file_exists($this->footer)) {
+			require $this->footer;
+		} else {
+			Debug::pagedump("The view: '$this->footer' does not exist");
+		}
+	}
+
+	/**
+	 * @param $view
+	 *
+	 * Renders the content
+	 * from the requested view
+	 */
+	public function render_view($view) {
+		if (file_exists($this->get_view_path($view))) {
+			require $this->get_view_path($view);
+		} else {
+			Debug::pagedump("The view: '". $this->get_view_path($view) ."' does not exist");
+		}
+	}
+
+	/**
+	 * @param $view
+	 * @return String
+	 *
+	 * returns the content
+	 * from the requested view
+	 */
+	public function get_view_path($view) {
+		return $this->basepath.$view.".php";
 	}
 }
--- views/index.php
+++ views/index.php
@@ -1,2 +1,4 @@
+<?php $this->extend("layout/header"); ?>
 <h1 id="title">Snail</h1>
 <h1>PHP Micro Framework</h1>
+<?php $this->extend("layout/footer"); ?>
