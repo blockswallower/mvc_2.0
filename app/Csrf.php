52daab2be14891f762806754ec026157dad31adc
Dennis <dennisslimmers@live.nl>
2016-10-09 12:30:37 +0200

    Updated Csrf class with more informative dumps instead of 'return false'

--- app/Csrf.php
+++ app/Csrf.php
@@ -1,95 +1,134 @@
 <?php
-// Must call session_start() before this loads
+/*
+ * Must call session_start() before this loads
+ */
 
-// Generate a token for use with CSRF protection.
-// Does not store the token.
+/*
+ * Generate a token for use with CSRF protection.
+ * Does not store the token.
+ */
 function csrf_token() {
-	return md5(uniqid(rand(), TRUE));
+	return md5(uniqid(rand(), true));
 }
 
-// Generate and store CSRF token in user session.
-// Requires session to have been started already.
+/*
+ * Generate and store CSRF token in user session.
+ * Requires session to have been started already.
+ */
 function create_csrf_token() {
 	$token = csrf_token();
+
 	$_SESSION['csrf_token'] = $token;
 	$_SESSION['csrf_token_time'] = time();
+
 	return $token;
 }
 
-// Destroys a token by removing it from the session.
+/*
+ * Destroys a token by removing it from the session.
+ */
 function destroy_csrf_token() {
 	$_SESSION['csrf_token'] = null;
 	$_SESSION['csrf_token_time'] = null;
+
 	return true;
 }
 
-// Return an HTML tag including the CSRF token 
-// for use in a form.
-// Usage: echo csrf_token_tag();
+/*
+ * Return an HTML tag including the CSRF token
+ * for use in a form.
+ * Usage: echo csrf_token_tag();
+ */
 function csrf_token_tag() {
 	$token = create_csrf_token();
+
 	return "<input type=\"hidden\" name=\"csrf_token\" value=\"".$token."\">";
 }
 
-// Returns true if user-submitted POST token is
-// identical to the previously stored SESSION token.
-// Returns false otherwise.
+/*
+ * Returns true if user-submitted POST token is
+ * identical to the previously stored SESSION token.
+ * Returns false otherwise.
+ */
 function csrf_token_is_valid() {
 	if (isset($_POST['csrf_token'])) {
 		$user_token = $_POST['csrf_token'];
 		$stored_token = $_SESSION['csrf_token'];
+
 		return $user_token === $stored_token;
 	} else {
-		return false;
+		Debug::pagedump("POST CSRF token is not identical to stored SESSION", __LINE__, __CLASS__);
 	}
 }
 
-// You can simply check the token validity and 
-// handle the failure yourself, or you can use 
-// this "stop-everything-on-failure" function. 
+/*
+ * You can simply check the token validity and
+ * handle the failure yourself, or you can use
+ * this "stop-everything-on-failure" function.
+ */
 function die_on_csrf_token_failure() {
 	if (!csrf_token_is_valid()) {
-		die("CSRF token validation failed.");
+		Debug::pagedump("CSRF token validation failed", __LINE__, __CLASS__);
 	}
 }
 
-// Optional check to see if token is also recent
+/*
+ * Optional check to see if token is also recent
+ */
 function csrf_token_is_recent() {
-	$max_elapsed = 60 * 60 * 24; // 1 day
+	/*
+	 * @var Integer
+	 * 1 day
+	 */
+	$max_elapsed = 60 * 60 * 24;
+
 	if (isset($_SESSION['csrf_token_time'])) {
 		$stored_time = $_SESSION['csrf_token_time'];
 		return ($stored_time + $max_elapsed) >= time();
 	} else {
-		// Remove expired token
+		/*
+		 * Remove expired token
+		 */
 		destroy_csrf_token();
-		return false;
+		Debug::pagedump("CSRF Token is not recent!", __LINE__, __CLASS__);
 	}
 }
 
-// GET requests should not make changes
-// Only POST requests should make changes
+/*
+ * GET requests should not make changes
+ * Only POST requests should make changes
+ */
 function request_is_get() {
 	return $_SERVER['REQUEST_METHOD'] === 'GET';
 }
 
+/*
+ * POST requests should not make changes
+ * Only GET requests should make changes
+ */
 function request_is_post() {
 	return $_SERVER['REQUEST_METHOD'] === 'POST';
 }
 
+/*
+ * Validates request
+ */
 function validate_request() {
 	if (request_is_post()) {
 		if (csrf_token_is_valid()) {
 			if (csrf_token_is_recent()) {
 				return true;
 			} else {
-				return false;
+				Debug::pagedump("CSRF Token is not recent!", __LINE__, __CLASS__);
 			}
 		} else {
-			return false;
+			Debug::pagedump("CSRF Token is not valid!", __LINE__, __CLASS__);
 		}
 	} else {
-		// form not submitted or was GET request
-		return false;
+		/*
+		 * form not submitted or was GET request
+		 */
+		Debug::pagedump("Form not submitted or was GET request", __LINE__, __CLASS__);
 	}
 }
 ?>
