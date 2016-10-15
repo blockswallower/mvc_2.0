cd06a3dc6f7330c1f0688080f874ae25ad2b834c
Dennis <dennisslimmers@live.nl>
2016-10-15 23:31:36 +0200

    Code cleanup Link helper class

--- helpers/Link.php
+++ helpers/Link.php
@@ -25,33 +25,30 @@ class Link {
 
     /**
      * @param $css_file
+     *
+     * Generates a stylesheet link
      */
      public static function style($css_file) {
-          /**
-           * Generates a stylesheet link
-           */
-          echo '<link href="'.self::$absolute_style_path.$css_file.'" rel="stylesheet">'."\n";
+          echo '<link href="' . self::$absolute_style_path.$css_file . '" rel="stylesheet">' . "\n";
      }
 
     /**
      * @param $js_file
+     *
+     * Generates a javascript
+     * file link
      */
      public static function script($js_file) {
-          /**
-           * Generates a javascript
-           * file link
-           */
-          echo '<script src="'.self::$absolute_script_path.$js_file.'"></script>'."\n";
+          echo '<script src="' . self::$absolute_script_path . $js_file . '"></script>' . "\n";
      }
 
     /**
      * @param $font
+     *
+     * Generates a google font style tag
      */
     public static function google_font($font) {
-        /**
-         * Generates a google font style tag
-         */
-        echo '<link href="'.$font.'" rel="stylesheet">'."\n";
+        echo '<link href="' . $font . '" rel="stylesheet">'."\n";
     }
 
     /**
@@ -61,8 +58,13 @@ class Link {
      *
      * generates img tag
      */
-    public static function img($src, $width, $height, $css) {
-        $src = self::$absolute_img_path.$src;
-        echo '<img src="'.$src.'" class="'.$css.'" width="'.$width.'" height="'.$height.'">'."\t";
+    public static function img($src, $width, $height, $css = null) {
+        /*
+         * @var String
+         */
+        $src = self::$absolute_img_path . $src;
+
+        echo '<img src="' . $src . '" class="' . empty($css) ? "" : $css . '"
+               width="' . $width . '" height="' . $height.'">' . "\t";
     }
 }
