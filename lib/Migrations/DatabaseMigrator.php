96d25f84f7753282c00e79c930a015843200d4c2
Dennis <dennisslimmers@live.nl>
2016-10-25 11:25:16 +0200

    Moved PDO config to set_database_requirements method

--- lib/Migrations/DatabaseMigrator.php
+++ lib/Migrations/DatabaseMigrator.php
@@ -1,29 +1,35 @@
 <?php
 
 class DatabaseMigrator {
-    /**
+    /*
      * @var String
      * Database type
      */
     private $db_type;
 
-    /**
+    /*
      * @var String
      * Database host
      */
     private $db_host;
 
-    /**
+    /*
      * @var String
      * Database username
      */
     private $db_username;
 
-    /**
+    /*
      * @var String
      * Database password
      */
     private $db_password;
+
+    /*
+     * @var Object
+     * PDO Configuration
+     */
+    private $PDO;
 
     /**
      * @param $db_name
@@ -32,26 +38,20 @@ class DatabaseMigrator {
      */
     public function create_database($db_name) {
         /*
-         * @var Object
-         * PDO Configuration
-         */
-        $PDO = new PDO("$this->db_type:host=$this->db_host", $this->db_username, $this->db_password);
-
-        /*
          * Set PDO error mode to exception
          */
-        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $db_name = "`".str_replace("`", "``", $db_name) . "`";
 
         /*
          * Create the database!
          */
-        if ($PDO->query("CREATE DATABASE IF NOT EXISTS $db_name")) {
+        if ($this->PDO->query("CREATE DATABASE IF NOT EXISTS $db_name")) {
             echo "Database: '$db_name' has been successfully created! \n";
         }
 
-        $PDO->query("use $db_name");
+        $this->PDO->query("use $db_name");
     }
 
     /**
@@ -67,5 +67,11 @@ class DatabaseMigrator {
         $this->db_host = $db_host;
         $this->db_username = $db_username;
         $this->db_password = $db_password;
+
+        /*
+        * @var Object
+        * PDO Configuration
+        */
+        $this->PDO = new PDO("$this->db_type:host=$this->db_host", $this->db_username, $this->db_password);
     }
 }
