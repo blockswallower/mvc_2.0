0e3cdac8b31572930d07f30667c0c9d704d088b3
Dennis <dennisslimmers@live.nl>
2016-11-04 17:38:48 +0100

    Small fix (@authors -> @author)

--- app/Constants.php
+++ app/Constants.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package Snail_MVC
+ * @author Dennis Slimmers, Bas van der Ploeg
+ * @copyright Copyright (c) 2016 Dennis Slimmers, Bas van der Ploeg
+ * @link https://github.com/dennisslimmers01/Snail-MVC
+ * @license Open Source MIT license
+ */
 
 /**
  * Any constant can be defined in this
@@ -9,7 +16,7 @@
  */
 define("__URL__", "http://localhost/Snail-MVC/");
 
-/*
+/**
  * Bool is true if ajax call is made
  */
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
