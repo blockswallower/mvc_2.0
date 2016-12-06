7367282656504eac9c093a9b07e320f128b58dd6
blockswallower <dennisslimmers@live.nl>
2016-12-06 15:52:49 +0100

    Added more documentation to the routing mechanic(http/routes.php)

--- http/Routes.php
+++ http/Routes.php
@@ -12,6 +12,21 @@ class Routes {
          * information
          *
          * for example: "index" => "IndexController.show",
+         *
+         * Endless routing is also allowed:
+         *
+         * for example: "users/profile" => "ProfileController.show"
+         *
+         * If you would like the last URI argument to be
+         * a argument to your HTTP Request method, add this as the
+         * last URI argument:
+         *
+         * "users/{param} => "ProfileController.show"
+         *
+         * And add this to your HTTP request method:
+         *
+         * public function show($[PARAMETER_NAME]) {
+         *
          */
         $this->routes = [
              "index" => "IndexController.show",
