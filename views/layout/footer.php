60fa3c95d160eb05533bf5da23b4f8b992cb7ca2
Dennis <dennisslimmers@live.nl>
2017-03-05 21:37:54 +0100

    Removed config object (footer.php)
    Fix #24

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,11 +1,2 @@
-
-    <?php Link::script('script.js'); ?>
-
-    <script type="text/javascript">
-        config = {};
-        config.debug = true;
-        config.every_time_execution = "none";
-        config.one_time_execution = "none";
-    </script>
-    </body>
+</body>
 </html>
