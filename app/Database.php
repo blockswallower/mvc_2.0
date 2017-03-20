97af09bb781aa85f07e51c561ce51007c2b515e4
Dennis <dennisslimmers@live.nl>
2017-03-20 22:43:43 +0100

    Created seperate database_exists method (app/Database.php)

--- app/Database.php
+++ app/Database.php
@@ -42,27 +42,8 @@ class Database extends PDO {
         $this->db_password = Config::get("DB_PASSWORD");
 
         if (!empty($this->db_name)) {
-            /**
-             * create a temporary PDO object to test
-             * database connections
-             */
-            $PDO = new PDO("$this->db_type:host=$this->db_host", $this->db_username, $this->db_password);
-
-            $result = $PDO->prepare("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '"
-                                     . $this->db_name ."'");
-            $result->execute();
-
-            /**
-             * Returns an array if the given database exists
-             * Data should look like this:
-             *
-             * Array ( [0] => Array ( [SCHEMA_NAME] => snail [0] => snail ) )
-             */
-            $found_database = $result->fetchAll();
-
-            if (empty($found_database)) {
+            if (!$this->database_exists()) {
                 $data = "The database: <i>'" . $this->db_name . "'</i> does not exist!";
-
                 Debug::exitdump($data, __LINE__, "app/Database");
             }
         }
@@ -70,5 +51,36 @@ class Database extends PDO {
         $dsn = "$this->db_type:dbname=$this->db_name;host=$this->db_host";
 
         parent::__construct($dsn, $this->db_username, $this->db_password);
+    }
+
+    /*
+     * Checks if database exists
+     */
+    private function database_exists() {
+        $exists = true;
+
+        /**
+         * create a temporary PDO object to test
+         * database connections
+         */
+        $PDO = new PDO("$this->db_type:host=$this->db_host", $this->db_username, $this->db_password);
+
+        $result = $PDO->prepare("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '"
+            . $this->db_name ."'");
+        $result->execute();
+
+        /**
+         * Returns an array if the given database exists
+         * Data should look like this:
+         *
+         * Array ( [0] => Array ( [SCHEMA_NAME] => snail [0] => snail ) )
+         */
+        $found_database = $result->fetchAll();
+
+        if (empty($found_database)) {
+            $exists = false;
+        }
+
+        return $exists;
     }
 }
