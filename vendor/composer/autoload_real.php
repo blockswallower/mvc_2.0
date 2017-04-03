3871d651a64f50e16bfb3dfee41c070ee8e9fbe3
Dennis <dennisslimmers@live.nl>
2017-04-03 22:23:08 +0200

    Symfony CLI updated to v3.0.2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit0bd6f17a6f1073c59aa1f718fb297dfd
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit0bd6f17a6f1073c59aa1f718fb297dfd', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit0bd6f17a6f1073c59aa1f718fb297dfd
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit0bd6f17a6f1073c59aa1f718fb297dfd::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire0bd6f17a6f1073c59aa1f718fb297dfd($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire0bd6f17a6f1073c59aa1f718fb297dfd($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
